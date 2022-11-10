e0b429df61afd3c2e84a29906db03e69ef24b6fd
André Sanguinetti <andre.sanguinetti@estsetubal.ips.pt>
2022-11-10 18:15:32 +0000

    Initial commit

---
+++ src/com/pa/patterns/memento/model/ShoppingCart.java
@@ -0,0 +1,45 @@
+package com.pa.patterns.memento.model;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Date;
+import java.util.List;
+
+public class ShoppingCart{
+    private List<Product> products;
+
+    public ShoppingCart() {
+        products = new ArrayList<>();
+    }
+
+    public void addProduct(Product p) {
+        products.add(p);
+    }
+
+    public void reset() {
+        products.clear();
+    }
+
+    public void removeProduct(Product p) {
+        products.remove(p);
+    }
+
+    public Collection<Product> getProducts() {
+        return products;
+    }
+
+    public double getTotal() {
+        double total = 0;
+        for (Product p : products) {
+            total += p.getCost();
+        }
+        return total;
+    }
+
+    @Override
+    public String toString() {
+        return String.valueOf(products);
+    }
+
+
+}
